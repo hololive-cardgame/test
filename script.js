db3a7bcb4c56bec42ac73a61c9f3334815e5d881
hololive-cardgame <ejo3ru@gmail.com>
2025-03-21 20:04:21 +0800

    修正清除篩選按鈕

--- script.js
+++ script.js
@@ -158,14 +158,26 @@ function displayCards(cards) {
 
 // 清除篩選條件
 clearFiltersBtn.addEventListener('click', () => {
-    keywordSelect.value = '';
-    typeSelect.value = '';
-    attributeSelect.value = '';
-    tagSelect.value = '';
-    setSelect.value = '';
-    
-    // 顯示所有卡牌
-    displayCards(cardsData);
+    // 檢查是否有任何篩選條件被選擇
+    const isAnyFilterSelected = keywordSelect.value ||
+                                typeSelect.value ||
+                                attributeSelect.value ||
+                                tagSelect.value ||
+                                setSelect.value;
+
+    if (isAnyFilterSelected) {
+        // 如果有篩選條件被選擇，則清除所有篩選條件
+        keywordSelect.value = '';
+        typeSelect.value = '';
+        attributeSelect.value = '';
+        tagSelect.value = '';
+        setSelect.value = '';
+        clearKeywordBtn.style.display = 'none'; // 隱藏 "X"
+        
+        // 顯示所有卡牌
+        displayCards(cardsData);
+    }
+
 });
 
 // 顯示卡牌詳細資訊
