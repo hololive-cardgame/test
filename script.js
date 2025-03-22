a5edaa086528338d49ec538bff15738b2b96294b
hololive-cardgame <ejo3ru@gmail.com>
2025-03-22 23:08:20 +0800

    引入 Select2 的 CSS 文件6

--- script.js
+++ script.js
@@ -72,6 +72,12 @@ keywordSelect.innerHTML = '';
     // 設定預設為空值（選單本身保持空）
     keywordSelect.value = "";
 
+    // 使用 Select2 初始化
+    $(keywordSelect).select2({
+        // placeholder: "請選擇關鍵字",
+        // allowClear: true
+    });
+
 // 類型不用清空下拉選單
     // 填充類型選項
     types.forEach(type => {
@@ -131,6 +137,7 @@ setSelect.innerHTML = '';
     });
     // 設定預設為空值（選單本身保持空）
     setSelect.value = "";
+
 }
 
 // 根據篩選條件顯示卡牌
